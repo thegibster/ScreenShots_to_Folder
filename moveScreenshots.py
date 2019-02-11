9caf1277392e2db4ca1f298d2451ee0a5877581c
andrew gibbs <andrewgibbs@outlook.com>
2019-02-11 00:08:16 -0800

    linux and darwin support added

--- moveScreenshots.py
+++ moveScreenshots.py
@@ -1,48 +1,60 @@
 import os, re, shutil
+import platform
 
-user_dir = "~/Desktop/"
-screenshotRegex = re.compile (r'Screen Shot+')
+OS_SYSTEMS = {
+    'Linux':'Screenshot',
+    'Darwin':'Screen Shot+',
+    'Windows':'n' # Windows doesn't save its own screenshot file
+}
 
-try:
-    homedir = os.path.join(os.environ['HOME'], 'Desktop')
-    # print(homedir)
-    files_to_move = list(filter(screenshotRegex.match, os.listdir(homedir)))
-    # print(os.listdir(homedir))
-    # print(files_to_move)
-    # to move these Screen Shot files , the next line is used
+screenshotRegex = re.compile (r'{}'.format(OS_SYSTEMS[platform.system()]))
 
-    def move_the_files():
-        for x in list(files_to_move):
-            move_file = ('{}{}{}'.format(homedir,"/",x))
-            print(move_file)
-        
-            if os.path.isfile(move_file):
-                try:
-                    shutil.move(move_file, '{}{}'.format(homedir,"/ScreenShots"))
-                    print("File moved successfully")
-                except Exception as exception_object:
-                    print("Unexpected exception:,", exception_object)
+def main():
+    try:
+        homedir = os.path.join(os.environ['HOME'], 'Desktop')
+        # print(homedir)
+        files_to_move = list(filter(screenshotRegex.match, os.listdir(homedir)))
+        # print(os.listdir(homedir))
+        # print(files_to_move)
+        # to move these Screen Shot files , the next line is used
 
-    if not os.path.exists('{}{}'.format(homedir,"/ScreenShots")):
-        print("path does not exist")
-        try:
-            os.mkdir('{}{}'.format(homedir,"/ScreenShots"))
-        except Exception as exception_object:
-            print("Unexpected exception:,", exception_object)
+        def move_the_files():
+            if len(files_to_move):
+                for x in list(files_to_move):
+                    move_file = ('{}{}{}'.format(homedir,"/",x))
+                    print(move_file)
+
+                    if os.path.isfile(move_file):
+                        try:
+                            shutil.move(move_file, '{}{}'.format(homedir,"/ScreenShots"))
+                            print("File moved successfully")
+                        except Exception as exception_object:
+                            print("Unexpected exception:,", exception_object)
+            else:
+                print("No Screenshots available for moving")
+        if not os.path.exists('{}{}'.format(homedir,"/ScreenShots")):
+            print("path does not exist")
+            try:
+                os.mkdir('{}{}'.format(homedir,"/ScreenShots"))
+            except Exception as exception_object:
+                print("Unexpected exception:,", exception_object)
+            else:
+                move_the_files()
+
         else:
-            move_the_files()
-        
-    else:
-        print("path exists", '{}{}'.format(homedir,"/ScreenShots"))
-        try:
-           move_the_files()
-        except Exception as exception_object:
-            print("Unexpected exception:,", exception_object)
-       
+            print("path exists", '{}{}'.format(homedir,"/ScreenShots"))
+            try:
+               move_the_files()
+            except Exception as exception_object:
+                print("Unexpected exception:,", exception_object)
 
 
-    
-except Exception as exception_object:
-    print("Unexpected exception:,", exception_object)
-# os.path.isdir  to check if the directory exists
-# with open(file_path, mode) as file -- safe way to open files so they auto close on failure
+
+
+    except Exception as exception_object:
+        print("Unexpected exception:,", exception_object)
+    # os.path.isdir  to check if the directory exists
+    # with open(file_path, mode) as file -- safe way to open files so they auto close on failure
+
+if __name__ == '__main__':
+   main()
